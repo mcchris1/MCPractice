10bff6c23fb0ed78aa801aa78c56d78c107ded85
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-05-26 18:29:52 -0400

    14

---
+++ MCRepl/14-from_pairs.js
@@ -0,0 +1,29 @@
+/*-----------------------------------------------------------------
+Challenge: 14-fromPairs
+Difficulty: Intermediate
+Prompt:
+- Write a function named fromPairs that creates an object from an array containing nested arrays.
+- Each nested array will have two elements representing key/value pairs used to create key/value pairs in an object to be returned by the function.
+- If a key appears in multiple pairs, the rightmost pair should overwrite previous the previous entry in the object.
+Examples:
+fromPairs([ ['a', 1], ['b', 2], ['c', 3] ]) //=> { a: 1, b: 2, c: 3 }
+fromPairs([ ['name', 'Sam"], ['age', 24], ['name', 'Sally'] ]) //=> { name: "Sally", age: 24 }
+-----------------------------------------------------------------*/
+// Your solution for 14-fromPairs here:
+
+/*--- using forEach ---*/
+function fromPairs(arr) {
+    let obj = {};
+    arr.forEach(function(kvArr) {
+      obj[kvArr[0]] = kvArr[1];
+    });
+    return obj;
+  }
+  
+  /*--- using reduce & arrow function ---*/
+  // function fromPairs(arr) {
+  //   return arr.reduce((obj, kvArr) => {
+  //       obj[kvArr[0]] = kvArr[1];
+  //       return obj;
+  //   }, {});
+  // }
